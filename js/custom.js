6aef82b72bd6e0580e2000bb1a2392ecabe5fd85
tranvannam98 <tonhanhgia98@gmail.com>
2022-01-18 13:03:21 +0700

    update popup

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,19 @@ $('#siteNav').affix({
 		top: 100
 	}
 })
+
+
+
+window.addEventListener("load", function(){
+    setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
